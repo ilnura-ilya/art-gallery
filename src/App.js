d0af47625e0446825d296fa43303ed3d5b96721e
Ilnura <t.ilnura@gmail.com>
2022-01-24 15:32:35 -0700

    first message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+
+  const [art, setArt] = useState(0);
+
+  const {id, artist, image, name, year} = data[art];
+  
+  const previousItem = () => {
+    setArt ((art => {
+      art--;
+      if (art < 0){
+        return data.length -1;
+      }
+      return art;
+    }))
+  }
+
+  const nextItem = () => {
+    setArt ((art => {
+      art++;
+      if (art > data.length - 1){
+        art = 0;
+      }
+      return art;
+    }))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1>КАРТИНЫ</h1>
+      </div>
+      <div className='container'>
+        <h1>{id} - {artist}</h1>
+      </div>
+      <div className='container imageDiv'>
+        <img src={image} alt="artImage" width="400px"/>
+      </div>
+      <div className='container'>
+        <h2>{name} ,{year} г.</h2>
+      </div>
+      <div  className='buttons'>
+      <button className="btn" onClick={previousItem}>НАЗАД</button>
+      <button className="btn" onClick={nextItem}>ВПЕРЁД</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,61 @@
+import image from './Albert Birshtadt_Bereg morya v Nassau.jpg';
+import imageTwo from './Chaild Frederik Hassam  _ Most Old-Laima.jpg';
+import imageThree from './Clod Mone _ Cerkov Notr-Dam-de_Gras v Onflere.jpg';
+import imageFour from './Eduard Mane _ V oranjeree.jpg';
+import imageFive from './caillebotte-parusniki-v-arzhantee.jpg';
+import imageSix from './Martin Rico y Ortega _ Santa Maria Della Salute, Venice _1902.jpg';
+import imageSeven from './Pol Gogen _ Jenchiny Taiti.jpg';
+
+export const data = [
+   {
+      id: 1,
+      artist: "Альберт Бирштадт",
+      image: image,
+      name: "Берег моря в Нассау",
+      year: "--"
+   },
+   {
+      id: 2,
+      artist: "Фредерик Чайльд Гассам",
+      image: imageTwo,
+      name: "Мост у Олд-Лайма",
+      year: 1908
+
+   },
+   {
+      id: 3,
+      artist: "Клод Моне",
+      image: imageThree,
+      name: "Церковь Нотр-Дам-де-Грас в Онфлёре",
+      year: 1864
+   },
+   {
+      id: 4,
+      artist: "Эдуард Мане",
+      image: imageFour,
+      name: "В оранжерее",
+      year: 1879
+   },
+ 
+  {
+      id: 5,
+      artist: "Гюстав Кайботт",
+      image: imageFive,
+      name: "Парусники в Аржантее",
+      year: 1879
+   },
+   {
+      id: 6,
+      artist: "Мартин Рико-и-Ортега",
+      image: imageSix,
+      name: "Санта-Мария-делла-Салюте",
+      year: 1902
+   },
+   {
+      id: 7,
+      artist: "Поль Гоген",
+      image: imageSeven,
+      name: "Женщины Таити",
+      year: 1891
+   }
+]
